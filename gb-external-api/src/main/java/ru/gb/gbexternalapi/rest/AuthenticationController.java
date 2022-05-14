5f83cc0e74dd58b78b45029260fd1d6c70dbc257
Vitaly <woodanon123@gmail.com>
2022-05-14 12:46:18 +0500

    lesson 9 Добавил RequestInterceptor, непонятно как в нем получать бины сессии,OncePerRequestFilter непонятно как в нем передавать параметры в запрос. бин сессии с токенами реализован, непонятно в какой момент этот бин вызывать

--- gb-external-api/src/main/java/ru/gb/gbexternalapi/rest/AuthenticationController.java
+++ gb-external-api/src/main/java/ru/gb/gbexternalapi/rest/AuthenticationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.gb.gbapi.security.AuthenticationUserDto;
+import ru.gb.gbapi.security.api.AuthGateway;
 import ru.gb.gbexternalapi.entity.security.AccountUser;
 import ru.gb.gbexternalapi.security.jwt.JwtTokenProvider;
 import ru.gb.gbexternalapi.service.UserService;
@@ -20,14 +21,14 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/v1/auth")
-public class AuthenticationController {
+public class AuthenticationController implements AuthGateway {
 
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody AuthenticationUserDto authenticationUserDto) {
+    public ResponseEntity<AuthenticationUserDto> login(@RequestBody AuthenticationUserDto authenticationUserDto) {
         final String username = authenticationUserDto.getUsername();
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
@@ -37,11 +38,8 @@ public class AuthenticationController {
         Optional<AccountUser> accountUserOptional = userService.findByUsername(username);
         if (accountUserOptional.isPresent()) {
             String jwtToken = jwtTokenProvider.createToken(username, accountUserOptional.get().getRoles());
-
-            HashMap<Object, Object> response = new HashMap<>();
-            response.put("username", username);
-            response.put("token", jwtToken);
-            return ResponseEntity.ok(response);
+            authenticationUserDto.setToken(jwtToken);
+            return ResponseEntity.ok(authenticationUserDto);
         } else {
             throw  new BadCredentialsException("Invalid username or password");
         }
--- src/main/java/ru/gb/gbshopmart/service/CategoryService.java
+++ src/main/java/ru/gb/gbshopmart/service/CategoryService.java
@@ -16,21 +16,6 @@ import ru.gb.gbshopmart.web.dto.mapper.CategoryMapper;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import ru.gb.gbapi.category.dto.CategoryDto;
-import ru.gb.gbshopmart.dao.CategoryDao;
-import ru.gb.gbshopmart.entity.Category;
-import ru.gb.gbshopmart.web.dto.mapper.CategoryMapper;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -85,36 +70,5 @@ public class CategoryService {
         }
     }
 
-    private final CategoryDao categoryDao;
-    private final CategoryMapper categoryMapper;
 
-    @Transactional
-    public CategoryDto save(final CategoryDto categoryDto) {
-        Category category = categoryMapper.toCategory(categoryDto);
-        if (category.getId() != null) {
-            categoryDao.findById(categoryDto.getId()).ifPresent(
-                    (p) -> category.setVersion(p.getVersion())
-            );
-        }
-        return categoryMapper.toCategoryDto(categoryDao.save(category));
-    }
-
-
-    @Transactional(readOnly = true)
-    public CategoryDto findById(Long id) {
-        return categoryMapper.toCategoryDto(categoryDao.findById(id).orElse(null));
-    }
-
-
-    public List<CategoryDto> findAll() {
-        return categoryDao.findAll().stream().map(categoryMapper::toCategoryDto).collect(Collectors.toList());
-    }
-
-    public void deleteById(Long id) {
-        try {
-            categoryDao.deleteById(id);
-        } catch (EmptyResultDataAccessException e) {
-            log.error(e.getMessage());
-        }
-    }
 }
